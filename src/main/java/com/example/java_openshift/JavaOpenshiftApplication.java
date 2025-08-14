ef84a5e3ebf9dd0891e97de50024d0c4c68da075
mfayyadhr <rmuhammadfayyadh@gmail.com>
2025-08-14 09:36:59 +0700

    test

---
+++ src/main/java/com/example/java_openshift/JavaOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOpenshiftApplication.class, args);
+	}
+
+}
